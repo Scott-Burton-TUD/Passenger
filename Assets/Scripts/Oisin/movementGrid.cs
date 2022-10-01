af99b9239275885bf6fc23169f38586b586e739c
oisin <c20387516@mytudublin.ie>
2022-10-01 01:21:20 +0100

    collision

--- Assets/Scripts/Oisin/movementGrid.cs
+++ Assets/Scripts/Oisin/movementGrid.cs
@@ -7,25 +7,71 @@ public class movementGrid : MonoBehaviour
     private bool isMoving = false;
     private float speed = 0.2f;
     private Vector3 origPos, targetPos;
+    private bool uCol;
+    private bool dCol;
+    private bool lCol;
+    private bool rCol;
+
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) && !isMoving)
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), 1))
+        {
+            uCol = true;
+        }
+
+        else
+        {
+            uCol = false;
+        }
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 1))
+        {
+            dCol = true;
+        }
+
+        else
+        {
+            dCol = false;
+        }
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), 1))
+        {
+            lCol = true;
+        }
+
+        else
+        {
+            lCol = false;
+        }
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), 1))
+        {
+            rCol = true;
+        }
+
+        else
+        {
+            rCol = false;
+        }
+
+
+        if (Input.GetKey(KeyCode.UpArrow) && !isMoving && !uCol)
         {
             StartCoroutine(MovePlayer(Vector3.up));
         }
 
-        if (Input.GetKey(KeyCode.DownArrow) && !isMoving)
+        if (Input.GetKey(KeyCode.DownArrow) && !isMoving && !dCol)
         {
             StartCoroutine(MovePlayer(Vector3.down));
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow) && !isMoving)
+        if (Input.GetKey(KeyCode.LeftArrow) && !isMoving && !lCol)
         {
             StartCoroutine(MovePlayer(Vector3.left));
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) && !isMoving)
+        if (Input.GetKey(KeyCode.RightArrow) && !isMoving && !rCol)
         {
             StartCoroutine(MovePlayer(Vector3.right));
         }
